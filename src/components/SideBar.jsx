d7cbc655b9aac43d8a5be41afc02b3b6a075fc5b
Hannah Manes <baixinhamanes@hotmail.com>
2026-04-26 22:52:31 -0300

    sidebar pokedex logic made

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,9 +1,11 @@
 import Pokedex from "../pages/Pokedex";
 import { Link } from "react-router-dom";
-import { useState } from "react";
+import { useState, useContext } from "react";
+import { PokedexContext } from "../context/PokedexContext";
 
 function SideBar() {
-  const [isActive, setIsActive] = useState(true);
+  const { pokedex: pokemon } = useContext(PokedexContext);
+  const [isActive, setIsActive] = useState(false);
   console.log(isActive);
 
   function openSideBar() {
@@ -13,18 +15,26 @@ function SideBar() {
   return (
     <aside id="sidebar" className={`${isActive ? "open" : "close"}`}>
       <div className="pokedex-title">
+        <h2 className={`${isActive ? "open" : "close"}`}>Pokedex</h2>
         <button onClick={openSideBar}>
           <img
             src="../../public/icons/Pokedex.png"
             alt="Pokedex icon"
-            width={25}
+            width={35}
           />
         </button>
-        <h2 className={`${isActive ? "open" : "close"}`}>Pokedex</h2>
       </div>
-      <Pokedex />
-      <Link to="/pokedex" className={`${isActive ? "open" : "close"}`}>
-        Pokedex
+      {/* <Pokedex /> */}
+      {pokemon.map((poke, index) => (
+        <Link to={`pokemon/${poke.id}`} key={index} className="sidebar-poke">
+          <img src={poke.sprites.front_default} alt={`${poke.name} icon`} />
+          <h3>{poke.name}</h3>
+        </Link>
+      ))}
+      <Link
+        to="/pokedex"
+        className={`pokedex-link ${isActive ? "open" : "close"}`}>
+        Pokédex
       </Link>
     </aside>
   );
